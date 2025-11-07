7e9ec4951198bceb22f017480b7c8a31ab639b07
antonova-irina <antonova.mailbox@gmail.com>
2025-11-07 11:39:01 +0200

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+
+import { Component } from '@angular/core';
+import { Step2PersonTable } from './components/step2-person-table/step2-person-table';
+
+@Component({
+  selector: 'app-root',
+  imports: [Step2PersonTable],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  name = "Markos";
+}
